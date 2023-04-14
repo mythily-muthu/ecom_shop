a5141bb43ea45ac3c481274a5a08b2711c0b8fbf
mythily-muthu <mythilimuthu18@gmail.com>
2023-04-14 14:58:27 +0530

    added verify token

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -13,7 +13,7 @@ const verifyToken = (req, res, next) => {
             next();
         });
     } else {
-        return res.status(401).json("You are not authenticated!");
+        return res.status(401).json("You are not authenticated");
     }
 };
 
